5d3082af0f2d590fdd8c4045a3dcdd99d08562f9
Moonshield7 <fannie.p@hotmail.fr>
2023-04-12 14:41:51 +0200

    Etape 3 : Intégrer la page d'accueil
    Ajout des éléments manquants dans les cartes de photographe sur la page d'accueil.
    Ajout des attributs alt et aria-label.
    Ajout du style css correspondant

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,32 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price, id } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", `Portrait  de ${name}`);
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        h2.setAttribute("aria-label", "Nom du photographe");
+        const h3 = document.createElement('h3')
+        h3.textContent = `${city}, ${country}`
+        h3.setAttribute("aria-label", "Localisation");
+        const h4 = document.createElement('h4');
+        h4.textContent = tagline;
+        h4.setAttribute("aria-label", "slogan");
+        const p = document.createElement('p');
+        p.textContent = `${price}€/jour`
+        p.setAttribute("aria-label", "Prix");
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(h3);
+        article.appendChild(h4);
+        article.appendChild(p);
         return (article);
     }
     return { name, picture, getUserCardDOM }
 }
+

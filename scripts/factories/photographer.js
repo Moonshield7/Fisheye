86a7c27f79c1ddcc84b4be403db59dcf62b7f122
Moonshield7 <fannie.p@hotmail.fr>
2023-04-13 14:03:01 +0200

    Etape 4 : Gérer la navigation
    Finalisation de la page d'accueil
    Validation par AChecker
    ID du photographe récupéré via l'URL

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -5,26 +5,46 @@ function photographerFactory(data) {
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+
+        // Element a qui englobe la photo et le titre
+        const linkContainer = document.createElement('a');
+        linkContainer.setAttribute("href", `photographer.html?id=${id}`);
+        linkContainer.setAttribute("aria-label", name);
+
+        // Photo de profil
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture);
-        img.setAttribute("alt", `Portrait  de ${name}`);
+        img.setAttribute("alt", "");
+
+        // Nom du photographe
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        h2.setAttribute("aria-label", "Nom du photographe");
+
+        // Localisation
         const h3 = document.createElement('h3')
         h3.textContent = `${city}, ${country}`
-        h3.setAttribute("aria-label", "Localisation");
+        h3.setAttribute("aria-label", `Localisation : ${city}, ${country}`);
+        h3.setAttribute('tabindex', "0");
+
+        // Slogan
         const h4 = document.createElement('h4');
         h4.textContent = tagline;
-        h4.setAttribute("aria-label", "slogan");
-        const p = document.createElement('p');
-        p.textContent = `${price}€/jour`
-        p.setAttribute("aria-label", "Prix");
-        article.appendChild(img);
-        article.appendChild(h2);
+        h4.setAttribute("aria-label", `Slogan : ${tagline}`);
+        h4.setAttribute('tabindex', "0");
+
+        // Prix
+        const h5 = document.createElement('h5');
+        h5.textContent = `${price}€/jour`;
+        h5.setAttribute("aria-label", `Prix : ${price}€ par jour`);
+        h5.setAttribute('tabindex', "0");
+
+        // Ajout des éléments ci-dessus au DOM
+        article.appendChild(linkContainer);
+        linkContainer.appendChild(img);
+        linkContainer.appendChild(h2);
         article.appendChild(h3);
         article.appendChild(h4);
-        article.appendChild(p);
+        article.appendChild(h5);
         return (article);
     }
     return { name, picture, getUserCardDOM }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,5 @@
 //Mettre le code JavaScript lié à la page photographer.html
+const params = (new URL(document.location)).searchParams;
+let idPhotographer = params.get("id");
+
+console.log(idPhotographer);

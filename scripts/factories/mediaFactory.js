32d4f1099e1e671c0db9ae3759a63315b33b04e9
Moonshield7 <fannie.p@hotmail.fr>
2023-04-17 15:38:36 +0200

    Résolution de l'erreur
    Modification de la mediaFactory pour résoudre les erreurs en console lors de l'affichage des photos/videos dans la gallerie.

--- scripts/factories/mediaFactory.js
+++ scripts/factories/mediaFactory.js
@@ -1,58 +1,57 @@
 function mediaFactory(data){
     const {id, photographerId, title, image, video, likes, date, price} = data;
 
-    //Récupération des url des photos et vidéos
-    const imagePathUrl = `assets/Photos/${photographerId}/${image}`;
-    const videoPathUrl = `assets/Photos/${photographerId}/${video}`;
+        //Récupération des url des photos et vidéos
+        const imagePathUrl = `assets/Photos/${photographerId}/${image}`;
+        const videoPathUrl = `assets/Photos/${photographerId}/${video}`;
+        
+        // Création des éléments du DOM
+            // Container : article
+        const article = document.createElement('article');
+            // Lien qui contient l'image clickable
+        const imageLink = document.createElement('a');
+        imageLink.setAttribute("aria-label", `${title}`);
+            // Section de texte sous l'image
+        const pictureText = document.createElement('section')
+        pictureText.setAttribute("class", "picture-text");
+            // Titre de l'image
+        const pictureName = document.createElement('h3');
+        pictureName.textContent = `${title}`;
+        pictureName.setAttribute("tabindex", "0");
+        
+            // Nombre de likes de l'image
+        const pictureLikes = document.createElement('h3');
+        pictureLikes.textContent = `${likes} ♥`;
+        pictureLikes.setAttribute("tabindex", "0");
+        pictureLikes.setAttribute("aria-label", `${likes} likes - icône coeur`);
+        pictureLikes.setAttribute("class", "like-amount");
     
-    // Création des éléments du DOM
-        // Container : article
-    const article = document.createElement('article');
-        // Lien qui contient l'image clickable
-    const imageLink = document.createElement('a');
-    imageLink.setAttribute("aria-label", `${title}`)
-        // Si l'image est une photo :
-    const pictureContent = document.createElement('img');
-    pictureContent.src = imagePathUrl;
-        // Si l'image est une vidéo, on récupère une miniature
-    const videoContent = document.createElement('video');
-    videoContent.setAttribute("width", "300");
-    videoContent.setAttribute("height", "250");
-    videoContent.innerHTML = `<source src="${videoPathUrl}#t=0.1" type="video/mp4">`;
-        // Section de texte sous l'image
-    const pictureText = document.createElement('section')
-    pictureText.setAttribute("class", "picture-text");
-        // Titre de l'image
-    const pictureName = document.createElement('h3');
-    pictureName.textContent = `${title}`;
-    pictureName.setAttribute("tabindex", "0");
+        // Fonction qui permet d'afficher la gallerie
+        function getGallery() {
+            article.appendChild(imageLink);
+            // Si l'image est une vidéo, on récupère une miniature
+            if(data.video){
+                const videoContent = document.createElement('video');
+                videoContent.setAttribute("width", "300");
+                videoContent.setAttribute("height", "250");
+                videoContent.innerHTML = `<source src="${videoPathUrl}#t=0.1" type="video/mp4">`;
+                imageLink.appendChild(videoContent);
+                imageLink.href = videoPathUrl;
+            } 
+            // Si l'image est une photo :
+            if(data.image) {
+                const pictureContent = document.createElement('img');
+                pictureContent.src = imagePathUrl;
+                imageLink.appendChild(pictureContent);
+                imageLink.href = imagePathUrl;
+            }
     
-        // Nombre de likes de l'image
-    const pictureLikes = document.createElement('h3');
-    pictureLikes.textContent = `${likes} ♥`;
-    pictureLikes.setAttribute("tabindex", "0");
-    pictureLikes.setAttribute("aria-label", `${likes} likes - icône coeur`);
-    pictureLikes.setAttribute("class", "like-amount");
-
-    // Fonction qui permet d'afficher la gallerie
-    function getGallery() {
-        article.appendChild(imageLink);
-        if(video !== undefined){
-            imageLink.appendChild(videoContent);
-            imageLink.href = videoPathUrl;
-        } 
-        if(image !== undefined) {
-            imageLink.appendChild(pictureContent);
-            imageLink.href = imagePathUrl;
+            article.appendChild(pictureText);
+            pictureText.appendChild(pictureName);
+            pictureText.appendChild(pictureLikes);
+    
+            return article;
         }
-
-        article.appendChild(pictureText);
-        pictureText.appendChild(pictureName);
-        pictureText.appendChild(pictureLikes);
-
-        return article;
-    }
-
-    return { getGallery };
-
+    
+        return { getGallery };
 }
